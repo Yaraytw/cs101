7989870d330d556591827b9b93593fd33ebd042f
YaRay <144075561+Yaraytw@users.noreply.github.com>
2023-12-14 15:44:20 +0800

    Add files via upload

---
+++ EX2/main7.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int i = 2;
+    printf("%.5f", i *3.1415926535);
+    return 0;
+}
+
+
