5f7b656f14a4e5d23453c3edb18639f311ab59bd
YaRay <144075561+Yaraytw@users.noreply.github.com>
2023-12-18 02:34:26 +0800

    Add files via upload

---
+++ hw5/main1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n = 7;
+    for (int i=1 ; i<=n ; i++){
+        printf("%*c", n-i+1,' ');
+        for (int j=1 ; j<=i ; j++){
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
