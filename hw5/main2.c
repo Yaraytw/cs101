26e5828f0bb4a7cdb7a59aef60330a66cc1c8ddc
YaRay <144075561+Yaraytw@users.noreply.github.com>
2023-12-21 01:52:57 +0800

    Add files via upload

---
+++ hw5/main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n = 1000000;
+    double pi = 0;
+    for (int i = 0; i < n; i++) {
+        double x = 1.0 - (i % 2)*2; 
+        double y = 2*i + 1;
+        pi += x/y;
+    }
+    pi *= 4;
+    printf("%.5f\n", pi);
+
+    return 0;
+}
